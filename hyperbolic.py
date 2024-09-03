c1f3fc6ac5f7a3c144b7667084a83223db0185fa
HengchaoChen <71703302+HengchaoChen@users.noreply.github.com>
2024-09-03 11:47:34 -0400

    create files

---
+++ hyperbolic.py
@@ -0,0 +1,25 @@
+""" 
+Written by: Hengchao Chen
+Version: 0.1
+Last modified date: 2024-09-02
+Description: This file is used to define the module of hyperbolic spaces.
+"""
+
+import numpy as np
+
+# ---------------------- Hyperbolic space ---------------------- #
+
+def exp(base, vector):
+
+    pass
+
+def log(base, vector):
+
+    pass
+
+def dist(base, target):
+
+    pass
+
+# ---------------------- Random ---------------------- #
+
---
+++ spd.py
@@ -0,0 +1,24 @@
+""" 
+Written by: Hengchao Chen
+Version: 0.1
+Last modified date: 2024-09-02
+Description: This file is used to define the module of the space of symmetric positive definite matrices.
+"""
+
+import numpy as np
+
+# ---------------------- Symmetric positive definite space ---------------------- #
+
+def exp(base, vector):
+
+    pass
+
+def log(base, vector):
+
+    pass
+
+def dist(base, target):
+
+    pass
+
+# ---------------------- Random ---------------------- #
